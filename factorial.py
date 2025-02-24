5088a148be9f9b6b6a08b3e71ac4fa40c8761691
Ritika900277 <ritikadeybalsi@gmail.com>
2025-02-24 22:38:08 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,7 @@
+=int(input("enter the number"))
+factorial= 1
+
+for i in range(1,n+1):
+    factorial *= i
+
+print(f"the factorial of {n} is number {factorial}")
---
+++ syntax.py
@@ -0,0 +1,17 @@
+a=7
+print(type (a))
+b="Ritika"
+print(type(b))
+c=9.5
+print(type(c))
+d=True
+print(type(d))
+e=(7.8,5.6,14,"Ritika")
+print(type(e))
+f=[1,"Ritika",8.5]
+print(type(f))
+g={
+    "Ritika":72,
+    "Ritika":92
+}
+print(type(g))
